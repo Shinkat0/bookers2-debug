118b084f47e4f9c394f72342ee049e23bfd2b7cc
Shinkat0 <cntaro1234@gmail.com>
2020-03-19 06:55:28 +0000

    [add] favorites controller,model

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users,only: [:show,:index,:edit,:update]
   resources :books
+  resources :favorites,only: [:create, :destory]
   root 'home#top'
   get 'home/about'
 
